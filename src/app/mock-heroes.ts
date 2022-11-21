b1a396bfec1b97908650d53e0c672b4289c49d7d
Alan Jahir <alan.martinez5691@alumnos.udg.mx>
2022-11-21 12:08:10 -0600

    Add services

--- src/app/mock-heroes.ts
+++ src/app/mock-heroes.ts
@@ -1,13 +1,13 @@
 import { Hero } from './hero';
 
 export const HEROES: Hero[] = [
-  { id: 12, name: 'Dr. Nice' },
-  { id: 13, name: 'Bombasto' },
-  { id: 14, name: 'Celeritas' },
-  { id: 15, name: 'Magneta' },
-  { id: 16, name: 'RubberMan' },
-  { id: 17, name: 'Dynama' },
-  { id: 18, name: 'Dr. IQ' },
-  { id: 19, name: 'Magma' },
-  { id: 20, name: 'Tornado' }
+  { id: 12, name: 'Dr. Nice', points :  20},
+  { id: 13, name: 'Bombasto', points :  15 },
+  { id: 14, name: 'Celeritas', points :  10 },
+  { id: 15, name: 'Magneta', points :  12 },
+  { id: 16, name: 'RubberMan', points :  15 },
+  { id: 17, name: 'Dynama', points :  13 },
+  { id: 18, name: 'Dr. IQ', points :  16 },
+  { id: 19, name: 'Magma', points :  22 },
+  { id: 20, name: 'Tornado', points :  21 }
 ];
